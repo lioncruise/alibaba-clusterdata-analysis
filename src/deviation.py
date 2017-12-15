4b749916d02db7eee0eede24a5b9237e28cb9727
qinshijun <qinshijun16@otcaix.iscas.ac.cn>
2017-12-15 21:37:32 +0800

    初步统计分析CPU、内存、硬盘使用率方差随时间变化的情况

---
+++ src/deviation.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+
+    path = '..\dataset\server_usage.csv'
+
+    try:
+        f = open(path, 'r', encoding='utf-8')
+        data = pd.read_csv(f)
+        df = pd.DataFrame(data)
+        df.columns = ['timestamp', 'machine_id', 'cpu_usage', 'memory_usage', 'disk_usage', 'cpu_load_1', 'cpu_load_5',
+                      'cpu_load_15']
+        df_std_by_timestamp = df.groupby('timestamp').std()
+        df_std_by_timestamp.columns = ['machine_id', 'cpu_usage', 'memory_usage', 'disk_usage', 'cpu_load_1',
+                                       'cpu_load_5', 'cpu_load_15']
+        std_cpu_usage = df_std_by_timestamp['cpu_usage']
+        std_memory_usage = df_std_by_timestamp['memory_usage']
+        std_disk_usage = df_std_by_timestamp['disk_usage']
+        plt.plot(df_std_by_timestamp.index, std_cpu_usage, 'r', label='cpu')
+        plt.plot(df_std_by_timestamp.index, std_memory_usage, 'b', label='memory')
+        plt.plot(df_std_by_timestamp.index, std_disk_usage, 'y', label='disk')
+
+        plt.legend(bbox_to_anchor=[0.95, 1])
+        plt.title('server CPU, memory, disk usage standard deviation')
+        plt.xlabel('timestamp(s)')
+        plt.ylabel('standard deviation')
+        plt.grid()
+        plt.show()
+    except FileNotFoundError:
+        print('CSV file not found!')
+    finally:
+        print('finish')
