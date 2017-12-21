49e4e27533c547245129f692f600f736deb8248c
qinshijun <qinshijun16@otcaix.iscas.ac.cn>
2017-12-21 16:33:09 +0800

    统计物理机CPU利用率在时间、空间上的分布

---
+++ src/server_cpu_usage_in_time_and_space.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+
+if __name__ == '__main__':
+
+    path = '..\dataset\server_usage.csv'
+
+    try:
+        f = open(path, 'r', encoding='utf-8')
+        data = pd.read_csv(f)
+        df = pd.DataFrame(data)
+        df.columns = ['timestamp', 'machine_id', 'cpu_usage', 'memory_usage', 'disk_usage', 'cpu_load_1', 'cpu_load_5',
+                      'cpu_load_15']
+
+        df = df.pivot(index='machine_id', columns='timestamp', values='cpu_usage')
+
+        norm = mpl.colors.Normalize(vmin=0, vmax=100)  # 标准化2D图colorbar的范围
+        im = plt.imshow(df, interpolation='nearest', aspect='auto', norm=norm)
+        plt.gca().invert_yaxis()  # 默认的y轴坐标画出来是反的
+
+        # 将x轴的坐标转换成小时为单位
+        to_hour = lambda x, pos: x / 12
+        plt.gca().xaxis.set_major_formatter(FuncFormatter(to_hour))
+        plt.xticks((0, 24, 48, 72, 96, 120, 144))
+
+        plt.colorbar(im, fraction=0.015, pad=0.05).ax.set_ylabel('CPU usage (%)')
+        plt.title('server CPU usage in time and space')
+        plt.xlabel('time (h)')
+        plt.ylabel('machine (id)')
+        plt.show()
+
+    except FileNotFoundError:
+        print('CSV file not found!')
+    finally:
+        print('finish')
