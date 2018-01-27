a550c58f69e366dcc92be34fa0f58c7fdce6ef49
qinshijun <qinshijun16@otcaix.iscas.ac.cn>
2018-01-27 20:00:48 +0800

    修改column_name中container_usage中和CPU load average相关的列名

--- src/common/column_name.py
+++ src/common/column_name.py
@@ -4,8 +4,8 @@ server_usage_column_name = ['timestamp', 'machine_id', 'cpu', 'memory', 'disk', 
 # container_event.csv's last column is useless, but we have to write it to the list otherwise pandas goes to wrong.
 container_event_column_name = ['timestamp', 'event_type', 'instance_id', 'machine_id', 'cpu_requested',
                                'memory_requested', 'disk_requested', 'cpu_ids', 'useless_column']
-container_usage_column_name = ['timestamp', 'instance_id', 'cpu', 'memory', 'disk', 'cpu_1min', 'cpu_5min', 'cpu_15min',
-                               'avg_cycles_per_instruction', 'avg_cache_miss_per_1000_instruction',
+container_usage_column_name = ['timestamp', 'instance_id', 'cpu', 'memory', 'disk', 'cpu_load_1min', 'cpu_load_5min',
+                               'cpu_load_15min', 'avg_cycles_per_instruction', 'avg_cache_miss_per_1000_instruction',
                                'max_cycles_per_instruction', 'max_cache_miss_per_1000_instruction']
 batch_task_column_name = ['create_time', 'end_time', 'job_id', 'task_id', 'num_of_instances', 'status',
                           'cpu_per_instance', 'memory_per_instance']
