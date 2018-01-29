93a71201aeaea479d2968aee247675c95df6f1da
qinshijun <qinshijun16@otcaix.iscas.ac.cn>
2018-01-29 15:56:23 +0800

    统计在线服务容器12h内平均CPU、内存使用率的概率分布（PDF）

---
+++ src/container_mean_cpu_usage_pdf.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from src.common.column_name import container_usage_column_name
+
+# 统计在线服务容器平均12h内CPU资源使用率的概率分布（PDF）
+if __name__ == '__main__':
+
+    path = '..\dataset\container_usage.csv'
+
+    try:
+        f = open(path, 'r', encoding='utf-8')
+        data = pd.read_csv(f)
+        df = pd.DataFrame(data)
+        df.columns = container_usage_column_name
+        grp = df[['timestamp', 'instance_id', 'cpu']].groupby('instance_id')
+        mean_grp = grp.mean()
+
+        mean_cpu_usage_by_container = mean_grp['cpu']
+
+        fig, ax = plt.subplots(figsize=(5, 4))
+        num_bins = 100
+
+        ax.hist(mean_cpu_usage_by_container, num_bins, normed=1,  # histtype='step',
+                cumulative=False)
+
+        ax.grid(True)
+        # ax.legend(loc='right')
+        ax.set_title('PDF of container\'s mean CPU usage')
+        ax.set_xlabel('mean CPU usage during 12h of a container (%)')
+        ax.set_ylabel('portion of containers')
+        ax.set_xlim([-5, 100])
+
+        plt.show()
+
+    except FileNotFoundError:
+        print('CSV file not found!')
+    finally:
+        print('finish')
